6748ddd0a8b867f8d280e3ffd2dfe98653ca0e62
Mohamed <elmoufid.mohamed@outlook.com>
2020-12-02 12:14:17 +0100

    Extra Test Sounds + Play In Angle

--- Assets/Scripts/Sound360.cs
+++ Assets/Scripts/Sound360.cs
@@ -30,17 +30,42 @@ public class Sound360 : MonoBehaviour
 
         var angle = gameObject.transform.rotation.eulerAngles.y;
 
-        if ((angle < 45 && angle > 0) || (angle > 315 && angle < 360))
-            Debug.Log("Voorkant"  + angle );
+        if ((angle < 45 && angle > 0) || (angle > 315 && angle < 360)) {
+            Debug.Log("Voorkant" + angle);
+            frontAudioSource.volume = 1f;
+            backAudioSource.volume = 0f;
+            leftAudioSource.volume = 0f;
+            rightAudioSource.volume = 0f;
+        }
+
 
         if ((angle < 135 && angle > 45))
+        {
             Debug.Log("Rechts" + angle);
+            frontAudioSource.volume = 0f;
+            backAudioSource.volume = 0f;
+            leftAudioSource.volume = 0f;
+            rightAudioSource.volume = 1f;
+        }
 
         if ((angle < 225 && angle > 135))
+        {
+            frontAudioSource.volume = 0f;
+            backAudioSource.volume = 1f;
+            leftAudioSource.volume = 0f;
+            rightAudioSource.volume = 0f;
             Debug.Log("Achter" + angle);
+        }
+      
 
         if ((angle < 315 && angle > 225))
+        {
+            frontAudioSource.volume = 0f;
+            backAudioSource.volume = 0f;
+            leftAudioSource.volume = 1f;
+            rightAudioSource.volume = 0f;
             Debug.Log("Links" + angle);
+        }
 
 
 
