4b5ae03a0618f096844a045c743d0688e518c1ca
Mohamed <elmoufid.mohamed@outlook.com>
2020-12-02 12:03:45 +0100

    Indentify Side

--- Assets/Scripts/Sound360.cs
+++ Assets/Scripts/Sound360.cs
@@ -28,8 +28,25 @@ public class Sound360 : MonoBehaviour
     void Update()
     {
 
-        Debug.Log(gameObject.transform.rotation.eulerAngles.y);
+        var angle = gameObject.transform.rotation.eulerAngles.y;
+
+        if ((angle < 45 && angle > 0) || (angle > 315 && angle < 360))
+            Debug.Log("Voorkant"  + angle );
+
+        if ((angle < 135 && angle > 45))
+            Debug.Log("Rechts" + angle);
+
+        if ((angle < 225 && angle > 135))
+            Debug.Log("Achter" + angle);
+
+        if ((angle < 315 && angle > 225))
+            Debug.Log("Links" + angle);
+
+
+
+
+        //Debug.Log(gameObject.transform.rotation.eulerAngles.y);
     }
 
-    
+
 }
