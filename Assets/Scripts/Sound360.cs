1d209f8483f47ff3df9bbb98851296e228f796d6
Mohamed <elmoufid.mohamed@outlook.com>
2020-12-02 11:43:05 +0100

    Add Test Sound Improve Audio Script

--- Assets/Scripts/Sound360.cs
+++ Assets/Scripts/Sound360.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Sound360 : MonoBehaviour
 {
+    public AudioSource frontAudioSource;
+    public AudioSource backAudioSource;
+    public AudioSource leftAudioSource;
+    public AudioSource rightAudioSource;
+
+    private const int FrontAngle = 0;
+    private const int RightAngle = 90;
+    private const int BackAngle = 180;
+    private const int LeftAngle = 270;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        frontAudioSource.Play();
+        backAudioSource.Play();
+        leftAudioSource.Play();
+        rightAudioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Debug.Log(gameObject.transform.rotation.eulerAngles.y);
     }
+
+    
 }
