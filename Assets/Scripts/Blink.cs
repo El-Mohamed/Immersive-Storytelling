a07037bb58ac33b6a052782d83171120f4224763
Kerem <s100757@ap.be>
2020-12-03 17:54:15 +0100

    Blink toegevoegd.

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    public GameObject bottom;
+    public GameObject Top;
+    public float TimeLeft = 4;
+
+    private bool blinkOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (blinkOver)
+        {
+            Destroy(bottom);
+            Destroy(Top);
+        }
+
+        TimeLeft -= Time.deltaTime;
+        if (TimeLeft < 0)
+        {
+            blinkOver = true;
+        }
+
+
+
+
+
+    }
+}
