92439eb10df6ba1dbfa9bcb21c5075567c67c8f5
Kerem <s100757@ap.be>
2020-12-13 22:33:58 +0100

    Task afgewerkt

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextScene : MonoBehaviour
+{
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.gameObject.tag == "Player")
+        {
+            //SceneManager.LoadScene(nextSceneIndex);
+            Debug.Log("tagged");
+        }
+    }
+
+}
