dfa7e753e81304c1a1a792145f4edbbeafc49257
Kerem <s100757@ap.be>
2020-12-14 13:15:04 +0100

    timer next scene

---
+++ Assets/Scripts/TimerToNextScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerToNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float TimeLeft = 30;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        TimeLeft -= Time.deltaTime;
+        if (TimeLeft < 0)
+        {
+            SceneManager.LoadScene("brightworld");
+        }
+
+
+
+
+    }
+}
