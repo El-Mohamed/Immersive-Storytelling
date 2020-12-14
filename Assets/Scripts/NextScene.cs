7541abe08fc545490fbdfe2ed03fb5d4550ec88c
Kerem <s100757@ap.be>
2020-12-14 12:42:05 +0100

    Transitie toegevoegd

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class NextScene : MonoBehaviour
 
         if (collider.gameObject.tag == "Player")
         {
-            //SceneManager.LoadScene(nextSceneIndex);
+            SceneManager.LoadScene("brightworld");
             Debug.Log("tagged");
         }
     }
